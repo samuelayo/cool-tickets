a67fe85bd9168e0532d6c42923f0f65ea6259e73
samuelayo <ayoogundipe2005@gmail.com>
2017-05-18 14:26:35 +0100

    socket

--- public/js/app.js
+++ public/js/app.js
@@ -31400,7 +31400,7 @@ function getNestedChildren(arr, parent) {
       for (var i = 0; i < this.users.length; i++) {
         if (this.users[i].id == userid) {
 
-          return this.users[i];
+          return this.users[i].name;
         }
       }
     },
@@ -127769,7 +127769,7 @@ module.exports={render:function (){var _vm=this;var _h=_vm.$createElement;var _c
     }
   })], 1), _vm._v(" "), _c('div', {
     staticClass: "col-md-12"
-  }, [_c('br'), _c('br'), _c('br'), _vm._v(" "), (_vm.forum.length != 0) ? _c('div', {
+  }, [_c('br'), _c('br'), _c('br'), _vm._v(" "), (_vm.forum.id) ? _c('div', {
     staticClass: "col-md-9"
   }, [_c('div', {
     staticClass: "panel"
