6205818155381c3505668846cba08da20d19dc1f
samuelayo <ayoogundipe2005@gmail.com>
2017-05-18 15:32:40 +0100

    socket

--- socket.js
+++ socket.js
@@ -11,8 +11,8 @@ redis.subscribe('coolfm-lagos');
 redis.on('message', function(channel, message) {
     message = JSON.parse(message);
 
-    var emit = io.emit('coolfmlagos_' + message.event, message.data);
-    console.log('coolfmlagos_' + message.event);
+    var emit = io.emit('coolfmlagos_CommentMade', message.data);
+
 
 });
 
