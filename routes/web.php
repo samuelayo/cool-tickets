08575f5cb06b73aae2f5a5a3dae693dea752e455
samuelayo <ayoogundipe2005@gmail.com>
2017-06-08 16:15:49 +0100

    global cors

--- routes/web.php
+++ routes/web.php
@@ -31,97 +31,99 @@ Route::group(['prefix'=>'m'], function () {
 
 });
 
-Route::get('/home', 'HomeController@index');
-Route::get('/posts/{id}', 'HomeController@post');
-Route::get('/schedules', 'HomeController@schedules');
-Route::get('/oaps', 'HomeController@oaps');
-Route::get('/thisoap/{id}', 'HomeController@thisoap');
-Route::get('/allpodcasts', 'HomeController@allpodcasts');
-Route::get('/podcast/{week}', 'HomeController@weekpodcast');
-Route::get('blog_category/{id}', 'HomeController@blog_category');
+
+Route::group(['middleware'=>'cors'], function(){
+    Route::get('/home', 'HomeController@index');
+    Route::get('/posts/{id}', 'HomeController@post');
+    Route::get('/schedules', 'HomeController@schedules');
+    Route::get('/oaps', 'HomeController@oaps');
+    Route::get('/thisoap/{id}', 'HomeController@thisoap');
+    Route::get('/allpodcasts', 'HomeController@allpodcasts');
+    Route::get('/podcast/{week}', 'HomeController@weekpodcast');
+    Route::get('blog_category/{id}', 'HomeController@blog_category');
 
 
 
 
-Route::get('/charts', 'HomeController@charts');
-Route::get('post/{id}/comments', function($id){
-      return \App\Models\BlogPost::where('id',$id)->first()->comments;
-    });
+    Route::get('/charts', 'HomeController@charts');
+    Route::get('post/{id}/comments', function($id){
+          return \App\Models\BlogPost::where('id',$id)->first()->comments;
+        });
 
-    Route::post('post/{id}/comment',function(\App\Models\BlogPost $id, Request $request){
-      $user = auth()->user();
-       $id->comment([
-          'body' => $request->get('body'),
-         'parent_id' => $request->get('parent_id', null)
-      ],$user);
-      $all_comments=$id->comments;
-      $data = array('event'=>'CommentMade',
-                    'data'=>[
-                      'type'=>'blog',
-                      'id'=>$id,
-                      'all_comments'=>$all_comments
-                    ]);
-      Redis::publish('coolfm-lagos',json_encode($data));
-      return $all_comments;
-    });
+        Route::post('post/{id}/comment',function(\App\Models\BlogPost $id, Request $request){
+          $user = auth()->user();
+          $id->comment([
+              'body' => $request->get('body'),
+            'parent_id' => $request->get('parent_id', null)
+          ],$user);
+          $all_comments=$id->comments;
+          $data = array('event'=>'CommentMade',
+                        'data'=>[
+                          'type'=>'blog',
+                          'id'=>$id,
+                          'all_comments'=>$all_comments
+                        ]);
+          Redis::publish('coolfm-lagos',json_encode($data));
+          return $all_comments;
+        });
 
 
-    Route::get('auth/{provider}', 'Auth\LoginController@redirectToProvider');
-    Route::get('auth/{provider}/callback', 'Auth\LoginController@handleProviderCallback');
+        Route::get('auth/{provider}', 'Auth\LoginController@redirectToProvider');
+        Route::get('auth/{provider}/callback', 'Auth\LoginController@handleProviderCallback');
 
-    Route::get('getforums', function(){
-      return \App\Models\Forum::with('category_name', 'user', 'comments')->get();
-    });
+        Route::get('getforums', function(){
+          return \App\Models\Forum::with('category_name', 'user', 'comments')->get();
+        });
 
-    Route::get('thisforum/{id}', function($id){
-      return \App\Models\Forum::where('id',$id)->with('category_name', 'user', 'comments')->first();
-    });
+        Route::get('thisforum/{id}', function($id){
+          return \App\Models\Forum::where('id',$id)->with('category_name', 'user', 'comments')->first();
+        });
 
-    Route::get('myforum/{id}/comments', function($id){
-      return \App\Models\Forum::where('id',$id)->first()->comments;
-    });
+        Route::get('myforum/{id}/comments', function($id){
+          return \App\Models\Forum::where('id',$id)->first()->comments;
+        });
 
-    Route::post('myforum/{id}/comment',function($id, Request $request){
+        Route::post('myforum/{id}/comment',function($id, Request $request){
 
-      $user = auth()->user();
-       \App\Models\Forum::where('id',$id)->first()->comment([
-          'body' => $request->get('body'),
-         'parent_id' => $request->get('parent_id', null)
-      ],$user);
-      $all_comments=\App\Models\Forum::where('id',$id)->first()->comments;
-      $data = array('event'=>'CommentMade',
-                    'data'=>[
-                      'type'=>'forum',
-                      'id'=>$id,
-                      'all_comments'=>$all_comments
-                    ]);
-      Redis::publish('coolfm-lagos',json_encode($data));
-      return $all_comments;
-    });
+          $user = auth()->user();
+          \App\Models\Forum::where('id',$id)->first()->comment([
+              'body' => $request->get('body'),
+            'parent_id' => $request->get('parent_id', null)
+          ],$user);
+          $all_comments=\App\Models\Forum::where('id',$id)->first()->comments;
+          $data = array('event'=>'CommentMade',
+                        'data'=>[
+                          'type'=>'forum',
+                          'id'=>$id,
+                          'all_comments'=>$all_comments
+                        ]);
+          Redis::publish('coolfm-lagos',json_encode($data));
+          return $all_comments;
+        });
 
-    Route::get('hotforum', function(){
-      return  \App\Models\Forum::where( DB::raw('YEAR(created_at)'), '=', date('Y'))->with('comments')->get();
-      
-    });
+        Route::get('hotforum', function(){
+          return  \App\Models\Forum::where( DB::raw('YEAR(created_at)'), '=', date('Y'))->with('comments')->get();
+          
+        });
 
-    Route::get('all_users', function(){
-       return  \App\User::all();
-    });
+        Route::get('all_users', function(){
+          return  \App\User::all();
+        });
 
-    Route::get('shows', function(){
-        return \App\Models\Shows::with('user')->orderBy('created_at', "DESC")->get();
-    });
+        Route::get('shows', function(){
+            return \App\Models\Shows::with('user')->orderBy('created_at', "DESC")->get();
+        });
 
-    Route::get('related/{id}', function($id){
-      return \App\Models\BlogPost::whereHas('category', function ($query) use($id) {
-              $query->where('id', $id);
-          })->orderBy('created_at', "DESC")->take(5)->get();
-    });
-    Route::post('initiate_ticket_purchase', 'Admin\EventsController@initiate');
+        Route::get('related/{id}', function($id){
+          return \App\Models\BlogPost::whereHas('category', function ($query) use($id) {
+                  $query->where('id', $id);
+              })->orderBy('created_at', "DESC")->take(5)->get();
+        });
+        Route::post('initiate_ticket_purchase', 'Admin\EventsController@initiate');
+        
+        Route::post('ticket_purchased', 'Admin\EventsController@ticket_purchased');
+
     
-    Route::post('ticket_purchased', 'Admin\EventsController@ticket_purchased');
-
-    Route::group(['middleware'=>'cors'], function(){
         Route::get('all_tickets', 'Admin\EventsController@allticktes');
         Route::get('ticket/{id}/redeem', 'Admin\EventsController@redeem');
     });
