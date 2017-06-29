b62ae08ae10dbbe1c5e6d6b0c65c056ef4afb16b
samuelayo <ayoogundipe2005@gmail.com>
2017-06-29 11:56:41 +0100

    try

--- resources/views/mobileview/index.blade.php
+++ resources/views/mobileview/index.blade.php
@@ -59,9 +59,9 @@ if(localStorage.getItem('username')!=undefined){
 <!-- Resource jQuery --> 
 
 
-<script type="text/javascript" src="/mobile/dist/static/js/manifest.e7e15cf1bd13bc873620.js"></script>
+<script type="text/javascript" src="/mobile/dist/static/js/manifest.250f833492a73484e090.js"></script>
     <script type="text/javascript" src="/mobile/dist/static/js/vendor.f1b39929ba8f18812360.js"></script>
-    <script type="text/javascript" src="/mobile/dist/static/js/app.7bd601d5e5f2d82a9339.js"></script>
+    <script type="text/javascript" src="/mobile/dist/static/js/app.204e174eb86beb1aa68d.js"></script>
  <!--<script src="http://localhost:8080/app.js"></script> -->
 
 <script src="https://js.paystack.co/v1/inline.js"></script>
