80475a961caf628937d3ae07a2539123d3289d5a
samuelayo <ayoogundipe2005@gmail.com>
2017-07-26 14:25:21 +0100

    aa

--- resources/assets/js/components/blogpost.vue
+++ resources/assets/js/components/blogpost.vue
@@ -20,7 +20,7 @@
                     <h1 class="title">{{post.title}}</h1>
 
                     <div class="article">
-                        <P id="p-sh" class="p-body" v-if="keypoint.length != 0">Story Highlights</P>
+                        <P id="p-sh" class="p-body" v-if="keypoint">Story Highlights</P>
                         <P class="p-body" v-for="(keypoint, index) in keypoints" :key="index" v-html="keypoint"></P>
 
                     </div>
@@ -236,8 +236,7 @@ import comment from './comment.vue'
                     this.keypoints = str.match(/<p>.*?<\/p>/g);
                     var status = {
                         title: 'Cool FM Nigeria | ' + this.post.title,
-                        description: this.post.content,
-                        twitterimage: '/'+this.post.image
+                        description: this.post.content
                     }
                     this.$store.dispatch('SET_SEO', status);
                 })
@@ -323,8 +322,7 @@ import comment from './comment.vue'
                         this.sliderimages = JSON.parse(this.post.extra_images);
                         var status = {
                             title: 'Cool FM Nigeria | ' + this.post.title,
-                            description: this.post.content,
-                             twitterimage: '/'+this.post.image
+                            description: this.post.content
                         }
                         this.$store.dispatch('SET_SEO', status);
                     })
