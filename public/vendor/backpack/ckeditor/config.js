5b369f8c740ee4c77b40636c3c2bc6f67f809ffe
samuelayo <ayoogundipe2005@gmail.com>
2017-08-16 12:00:25 +0100

    test

--- public/vendor/backpack/ckeditor/config.js
+++ public/vendor/backpack/ckeditor/config.js
@@ -44,7 +44,7 @@ CKEDITOR.editorConfig = function(config) {
     config.language = 'en';
 
     // Se the most common block elements.
-    config.format_tags = 'p;h1;h2;h3;pre';
+    config.format_tags = 'p;h1;h2;h3;h4;h5;h6;pre;address;div;script;span';
 
     // Make dialogs simpler.
     config.removeDialogTabs = 'image:advanced;link:advanced';
