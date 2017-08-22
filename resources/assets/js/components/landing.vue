ed29bebadcdfae4df2fa50cde438e601144691d1
samuelayo <ayoogundipe2005@gmail.com>
2017-08-22 12:01:20 +0100

    s

--- resources/assets/js/components/landing.vue
+++ resources/assets/js/components/landing.vue
@@ -286,6 +286,8 @@
                      </div>
                      <hr>
                   </div> -->
+
+                  <iframe src="https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2Fcoolfmnigeria%2F&tabs=timeline&width=340&height=500&small_header=true&adapt_container_width=true&hide_cover=false&show_facepile=true&appId" width="100%" height="100%" style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowTransparency="true"></iframe>
                     <a  v-if="sidebar_chart[0]" :href="sidebar_chart[0].url" >
                      <adimage :src="'/'+sidebar_chart[0].image_url" width="100%" height="100%" isd="1" style="padding: 1em;"></adimage>
                   </a>
