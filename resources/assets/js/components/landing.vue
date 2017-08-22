2077185e376fc27af301f397e6327c469dc6eceb
samuelayo <ayoogundipe2005@gmail.com>
2017-08-22 12:37:17 +0100

    s

--- resources/assets/js/components/landing.vue
+++ resources/assets/js/components/landing.vue
@@ -289,7 +289,7 @@
 
                   <iframe src="https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2Fcoolfmnigeria%2F&tabs=timeline&width=340&height=500&small_header=true&adapt_container_width=true&hide_cover=false&show_facepile=true&appId" width="100%" height="450px" style="border:none;overflow:hidden;padding: 1em;" scrolling="no" frameborder="0" allowTransparency="true"></iframe>
 
-                    <a class="twitter-timeline" href="https://twitter.com/CoolFMNigeria">Tweets by CoolFMNigeria</a> 
+                    <a class="twitter-timeline" href="https://twitter.com/CoolFMNigeria" data-height="300">Tweets by CoolFMNigeria</a> 
                     <a  v-if="sidebar_chart[0]" :href="sidebar_chart[0].url" >
                      <adimage :src="'/'+sidebar_chart[0].image_url" width="100%" height="100%" isd="1" style="padding: 1em;"></adimage>
                   </a>
@@ -647,6 +647,10 @@
    padding: 1em;
    margin-top: 1.4em;
    color: #fff !important;
+}
+
+.timeline-Widget.timeline-Widget--edge {
+    padding: 1em;
 }
 </style>
 
