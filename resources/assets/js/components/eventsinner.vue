614a1d7421958e15cb8608ad7e41e4a76eda6e48
samuel ogundipe <ayoogundipe2005@gmail.com>
2018-04-30 22:37:35 +0100

    build app

--- resources/assets/js/components/eventsinner.vue
+++ resources/assets/js/components/eventsinner.vue
@@ -3,7 +3,7 @@
         <div class="col-12" style="max-height: 330px;">
           <center>
             <ins class="adsbygoogle"
-                style="display:block"
+                style="display:block; height: 330px;"
                 data-ad-format="autorelaxed"
                 data-ad-client="ca-pub-4448078976745099"
                 data-ad-slot="5292952536"></ins>
